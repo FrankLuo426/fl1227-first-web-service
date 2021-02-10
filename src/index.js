0ba1d5f3e2ba01a497aa224cd26d53cf73706436
FrankLuo426 <33915033+FrankLuo426@users.noreply.github.com>
2021-02-11 01:08:55 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+const query = require('querystring');
 
+const jsonHandler = require('./jsonResponses.js');
+const htmlHandler = require('./htmlResponses.js');
 
 
 const urlStruct = {
@@ -12,10 +15,7 @@ const http = require('http');
 
 // 2 - pull in URL and query modules (for URL parsing)
 const url = require('url');
-const query = require('querystring');
 
-const jsonHandler = require('./jsonResponses.js');
-const htmlHandler = require('./htmlResponses.js');
 
 // 3 - locally this will be 3000, on Heroku it will be assigned
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
